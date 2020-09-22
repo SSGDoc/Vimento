e2184256d84f4ddff1d5f2cf169362875816f55a
Lars Peter Gerald Sørensen <1068772@ucn.dk>
2020-09-22 23:08:01 +0200

    Oprettet XML fil der holder kordinater, og oprettet script der sætter markers på kortet ud fra kordinater

---
+++ Vimento/Scripts/MarkerScript.js
@@ -0,0 +1,16 @@
+﻿$.ajax({
+    url: "/XMLdata/Adresser.xml",
+    dataType: "xml",
+    success: function (data) {
+        $(data).find("destinations point").each(function () {
+
+            var lat = $(this).find("point lat").text();
+            var long = $(this).find("point long").text();
+
+
+        });
+    },
+    error: function () {
+
+    }
+});
