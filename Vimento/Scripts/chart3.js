1b1f4eb96e9f6883f3c1fc3cde42af71e6e573ce
kjaer77 <losninos>
2020-09-24 14:03:48 +0200

    lidt små ændringer chart.js

--- Vimento/Scripts/chart3.js
+++ Vimento/Scripts/chart3.js
@@ -2,25 +2,25 @@
     .then(makeChart);
 
 
-function makeChart(players) {
-    var navn = players.map(function (d) { return d.Navn });
-    var antalKunder = players.map(function (d) { return d.Kunder });
+function makeChart(teleselskaber) {
+    var navn = teleselskaber.map(function (d) { return d.Navn });
+    var antalKunder = teleselskaber.map(function (d) { return d.Kunder });
 
     var chart = new Chart('chart', {
         type: 'bar',
         options: {
             title: {
                 display: true,
-                text: 'Antal kunder fordelt på Teleselskaber'
+                text: 'Antal kunder fordelt på Teleselskaber - CSV fil'
             },
             maintainAspectRatio: true,
             legend: {
                 display: false
             },
-            
+
         },
         data: {
-            labels: ["Yousee", "Telia", "3", "Duka"],
+            labels: navn,
             datasets: [
                 {
                     data: antalKunder,
@@ -29,15 +29,15 @@ function makeChart(players) {
                         'rgba(54, 162, 235, 0.2)',
                         'rgba(255, 206, 86, 0.2)',
                         'rgba(75, 192, 192, 0.2)'
-                        
+
                     ],
                     borderColor: 'rgb(96, 96, 96)',
                     borderWidth: '1',
                     hoverBackgroundColor: 'rgb(255, 228, 138)'
-                   
+
                 }
             ]
         }
     });
-   
+
 }
