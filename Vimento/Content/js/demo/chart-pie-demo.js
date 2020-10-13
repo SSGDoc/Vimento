9cd6a3052ce8734bf20233b8eda997a1ada298e8
BuildTools <unconfigured@null.spigotmc.org>
2020-10-13 14:07:45 +0200

    Added info board

--- Vimento/Content/js/demo/chart-pie-demo.js
+++ Vimento/Content/js/demo/chart-pie-demo.js
@@ -7,9 +7,9 @@ var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
   type: 'doughnut',
   data: {
-    labels: ["Direct", "Referral", "Social"],
+    labels: ["Fiber", "Fiber-KabelTV-Net", "KabelTV-Net"],
     datasets: [{
-      data: [55, 30, 15],
+        data: [56.36, 24.75, 18.89],
       backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
       hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
       hoverBorderColor: "rgba(234, 236, 244, 1)",
