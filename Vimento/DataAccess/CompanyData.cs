7014e82d6974076895017fbd132fa46bb11745d7
BuildTools <unconfigured@null.spigotmc.org>
2021-01-04 13:39:21 +0100

    Ready for delivery

--- Vimento/DataAccess/CompanyData.cs
+++ Vimento/DataAccess/CompanyData.cs
@@ -12,7 +12,8 @@ namespace Vimento.DataAccess
         private string _connectionString;
         public CompanyData()
         {
-            _connectionString = "server=127.0.0.1;port=3306;uid=root;pwd=digital;database=datawarehouse";
+            //Insert password to database after "pwd="
+            _connectionString = "server=127.0.0.1;port=3306;uid=root; pwd=1234; database=datawarehouse";
             
         }
 
